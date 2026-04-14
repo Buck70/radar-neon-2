d977fe8bc7d53dbb6763b44ec9d54f8d10c6d538
Buck70 <32963712+Buck70@users.noreply.github.com>
2026-04-14 21:57:11 +0200

    Delete lib directory

--- lib/store.ts
+++
@@ -1,4 +0,0 @@
-export const initialArticles = [
-  { id: 1, category: 'monde', title: "Radar Néon Opérationnel", summary: "Fichiers correctement enregistrés.", heat: 98, time: 'Direct' }
-];
-export async function getArticles() { return initialArticles; }
